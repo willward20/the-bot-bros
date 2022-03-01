d4929268469c76517b395f714ddc07edcbd60b00
donalb227 <77681364+donalb227@users.noreply.github.com>
2022-03-01 12:17:09 -0600

    Add files via upload
    subscriber version from fall semester final, needs to be edited and possibly renamed

---
+++ subscriber_member_function.py
@@ -0,0 +1,71 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from gpiozero import PhaseEnableRobot, LED
+
+robot = PhaseEnableRobot(left=(24,12), right=(25,13))
+
+motor1=LED(23) #these two lines initialize the motors
+motor2=LED(22) #
+
+motor1.on() #These two lines activate the motor
+motor2.on() #
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            Twist,
+            '/turtle1/cmd_vel',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.linear.x)
+        self.get_logger().info('I heard: "%s"' % msg.angular.z)
+        if msg.linear.x > 0:
+            robot.forward(0.35)
+        elif msg.linear.x < 0:
+            robot.backward(0.35)
+        else:
+            if msg.angular.z > 0:
+                robot.right(0.35)
+            else:
+                robot.left(0.35)
+
+def main(args=None):
+     
+    
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+    
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
