9eb7da02964dbdbb212ceef99683114cb818cba7
Will Ward <willward1912@gmail.com>
2022-03-08 19:40:34 -0600

    New Plot

--- plotting.py
+++ plotting.py
@@ -9,7 +9,7 @@ linear_l = []
 linear_r = []
 
 # read in the data
-lines = np.loadtxt('P_control.txt', delimiter=',') 
+lines = np.loadtxt('P_control_0.11l_0.1r.txt', delimiter=',') 
 for line in lines:
     time_data.append(line[0]) # the first item in row is the time
     linear_l.append(line[1]) # the second item in row is left linear velocity
@@ -27,7 +27,7 @@ plt.scatter(time_data,linear_r,color='blue', label='Right Wheel')
 # label the axes etc
 ax.set_xlabel('Time (s)')
 ax.set_ylabel('Velocity (m/s)')
-ax.set_title('P Control on Bot with p = 0.1')
+ax.set_title('P Control on Bot with p_left = 0.11 and p_right = 0.1')
 plt.legend(loc = 'lower right') # legend location can be changed
 
-plt.savefig('P_control_0.1.png')
+plt.savefig('P_control_0.11l_0.1r.png')
