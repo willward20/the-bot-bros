94d8d3c3fd034a881d4b2212c2af3228791d56b4
Will Ward <willward1912@gmail.com>
2022-04-05 13:33:06 -0500

    Add files

---
+++ project_2/launch/display.launch.py
@@ -0,0 +1,36 @@
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
+import os
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='project_2').find('project_2')
+    default_model_path = os.path.join(pkg_share, 'urdf/the_bot.urdf')
+    
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    )
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+    robot_localization_node = launch_ros.actions.Node(
+         package='robot_localization',
+         executable='ekf_node',
+         name='ekf_filter_node',
+         output='screen',
+         parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                            description='Absolute path to robot urdf file'),
+        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='False',
+                                            description='Flag to enable use_sim_time'),
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        robot_localization_node
+    ])
