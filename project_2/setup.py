78c6aecb31ae65e93ce7fdd8c2cb807b09921489
Will Ward <willward1912@gmail.com>
2022-04-12 12:01:39 -0500

    Modify files Update ekf.yaml, package.xml, and setup.py. Everything runs now, but the ekf_filter node is not subscribed to /odom for some reason.

--- project_2/setup.py
+++ project_2/setup.py
@@ -1,4 +1,6 @@
+import os
 from setuptools import setup
+from glob import glob
 
 package_name = 'project_2'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join("launch", "*.py"))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join("urdf", "*.urdf")))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'odom_publisher = project_2.odom_publisher:main'
         ],
     },
 )
